1cf03d132fb297cd3d30ba0646aa7d163d8b6d3c
Joanna K <joannakleinrm@gmail.com>
2023-06-17 19:43:21 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { SearchBar } from './SearchBar/SearchBar';
 import { fetchImages } from "./api/api";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
@@ -7,112 +7,134 @@ import { Modal } from "./Modal/Modal";
 import { Loader } from "./Loader/Loader";
 
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    query: "",
-    pageNr: 1,
-    error: null,
-    showModal: null,
-    largeImageURL: "",
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [pageNr, setPageNr] = useState(1);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(null);
+  const [largeImageURL, setLargeImageURL] = useState("");
+ 
+  // state = {
+  //   images: [],
+  //   isLoading: false,
+  //   query: "",
+  //   pageNr: 1,
+  //   error: null,
+  //   showModal: null,
+  //   largeImageURL: "",
+  // };
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.setState({
-        images: [],
-        pageNr: 1,
-        error: null
-      });
+  // componentDidUpdate(_, prevState) {
+  //   if (prevState.query !== this.state.query) {
+  //     this.setState({
+  //       images: [],
+  //       pageNr: 1,
+  //       error: null
+  //     });
+  //   }
+  // }
+
+  useEffect(() => {
+    if (!query) return;
+
+    const getImages = async () => {
+      // const { query, pageNr } = this.state;
+      if (query.trim() === '') {
+        alert('Please, use search field! ')
+      }
+    
+      toggleLoader();
+
+      try {
+        const request = await fetchImages(query, pageNr);
+        // this.setState(({ images, pageNr }) => ({
+        //   images: [...images, ...request],
+        //   pageNr: pageNr + 1,
+        setImages(prevImages => [...prevImages, ...request]);
+      
+        if (request.length === 0) {
+          setError({ error: `${query} not found` })
+        }
+      } catch (err) {
+        setError({ error: err });
+      } finally {
+        toggleLoader();
+      }
     }
-  }
+    getImages();
+  }, [query, pageNr]);
 
-  handleInputChange = evt => {
+  
+
+  const handleInputChange = evt => {
     const { name, value } = evt.target;
     this.setState({ [name]: value });
   };
 
-  handleSearchQuerySubmit = (evt) => {
-    evt.preventDefault();
-    this.getImages();
+  const handleSearchQuerySubmit = newSearch => {
+    setQuery(newSearch);
+    setImages([]);
+    setPageNr(1);
+    setError(null);
+    setIsLoading(true);
   };
 
-  getImages = async () => {
-    const { query, pageNr } = this.state;
-    if (query.trim() === '') {
-      alert('Please, use search field! ')
-    }
-    
-    this.toggleLoader();
+  
+  // const onLoadMore = async() => {
+  //   const response = await fetchImages(
+  //     setQuery(),
+  //     setPageNr(prevPage => prevPage + 1),
+  //   );
+  //   return {
+  //     setImages([...images, ...response]),
+  //     setPageNr(prevPage => prevPage + 1)
+  //   };
+  // }
 
-    try {
-      const request = await fetchImages(query, pageNr);
-      this.setState(({ images, pageNr }) => ({
-        images: [...images, ...request],
-        pageNr: pageNr + 1,
-      }));
-      if (request.length === 0) {
-        this.setState({error: `${query} not found`})
-      }
-    } catch (err) {
-      this.setState({ error: err });
-    } finally {
-      this.toggleLoader();
-    }
-  }
+   const onLoadMore = () => {
+    setIsLoading(true);
+    setPageNr(prevPage => prevPage + 1);
+  };
+  
 
-  onLoadMore = async() => {
-    const response = await fetchImages(
-      this.state.query,
-      this.state.pageNr + 1
-    );
-    this.setState({
-      images: [...this.state.images, ...response],
-      pageNr: this.state.pageNr + 1,
-    });
+  const onOpenModal = evt => {
+    setLargeImageURL(evt.target.dataset.source );
+    toggleModal(!isLoading);
   };
 
-  onOpenModal = evt => {
-    this.setState({ largeImageURL: evt.target.dataset.source });
-    this.toggleModal();
+  const toggleLoader = () => {
+    setIsLoading(!isLoading); 
+    };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  toggleLoader = () => {
-    this.setState(({ isLoading }) => ({
-      isLoading: !isLoading,
-    }));
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  render() {
-    const { query, images, largeImageURL, isLoading, showModal, error } = this.state;
+    // const { query, images, largeImageURL, isLoading, showModal, error } = this.state;
 
     return (
       <div>
         <SearchBar
-          onSubmit={this.handleSearchQuerySubmit}
-          onSearchQueryChange={this.handleInputChange}
+          onSubmit={handleSearchQuerySubmit}
+          onSearchQueryChange={handleInputChange}
           value={query}
         />
         {error}
          {images.length > 0 && !error && (
-          <ImageGallery images={images} onOpenModal={this.onOpenModal} />
+          <ImageGallery images={images} onOpenModal={onOpenModal} />
         )}
         {isLoading && <Loader />}
         {!isLoading && images.length >= 12 && !error &&
-          (<Button onLoadMore={this.onLoadMore} />)}
+          (<Button onLoadMore={onLoadMore} />)}
         {showModal && (
-          <Modal onToggleModal={this.toggleModal}
+          <Modal onToggleModal={toggleModal}
             largeImageURL={largeImageURL} />
         )}
       </div>
     );
   }
-}
 
+
+export default App;
